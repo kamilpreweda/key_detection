4b94f772fdb2841068d74bdfd9687be58b601ac2
Kamil Preweda <preweda.kamil@o2.pl>
2019-07-24 18:45:03 +0200

    added js, img file, secret code and styles

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -16,7 +16,21 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
+const pressed = [];
+const secretCode = "kamil";
+const body = document.body;
+const paragraph = document.querySelector('.heisenberg');
+const img = document.createElement('img');
+img.src = '../assets/img/heisenberg.jpg'
 
-console.log(`Hello world!`);
+window.addEventListener('keyup', (e) => {
+  pressed.push(e.key);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
 
+  if (pressed.join('').includes(secretCode)) {
+    console.log(`That's god damn right`);
+    body.removeChild(paragraph);
+    body.appendChild(img);
+  }
+})
 
